bb122809b128305ed331dea4f342f0001f4594ec
Jeff <jeff.wong1990@gmail.com>
2022-04-07 15:03:50 +1000

    Moved upgraded core application into root folder

--- MyDeal.TechTest/Controllers/SettingsController.cs
+++ MyDeal.TechTest/Controllers/SettingsController.cs
@@ -1,20 +1,27 @@
-﻿using MyDeal.TechTest.Models;
-using System.Configuration;
-using System.Web.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MyDeal.TechTest.Services;
+using MyDeal.TechTest.Models;
+using System.Text.Json;
 
 namespace MyDeal.TechTest.Controllers
 {
     public class SettingsController : Controller
     {
+        private IConfiguration _configuration;
+
+        public SettingsController(IConfiguration iconfig)
+        {
+            _configuration = iconfig;
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
             return Json(new SettingsVm
             {
                 User = UserService.GetUserDetails("2")?.Data,
-                Message = ConfigurationManager.AppSettings["Settings:Message"]
-            }, JsonRequestBehavior.AllowGet);
+                Message = _configuration.GetValue<string>("Settings:Message")
+            }, new JsonSerializerOptions { PropertyNamingPolicy = null });
         }
     }
 }
