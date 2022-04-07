22a3f780d26c6e8abc02a7602f32d4c8e5d2c101
Jeff <jeff.wong1990@gmail.com>
2022-04-07 18:06:22 +1000

    Fixed unit tests, added mocks and removed redundant classes

--- MyDeal.TechTest.Services.Tests/UserServiceTests.cs
+++ MyDeal.TechTest.Services.Tests/UserServiceTests.cs
@@ -1,3 +1,10 @@
+using Moq;
+using Moq.Protected;
+using RichardSzalay.MockHttp;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace MyDeal.TechTest.Services.Tests
@@ -5,10 +12,27 @@ namespace MyDeal.TechTest.Services.Tests
     public class UserServiceTests
     {
         [Fact]
-        public void Test1()
+        public async void Test1()
         {
-            UserService.WebRequestFactory = url => new TestWebRequest("{ \"data\": { \"first_name\": \"First\" } }");
-            Assert.Equal("First", UserService.GetUserDetails("2").Data.FirstName);
+            // Setting up the mock response for the client
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("{ \"data\": { \"first_name\": \"First\" } }"),
+                });
+
+            var client = new HttpClient(mockHttpMessageHandler.Object);
+            // Client BaseAddress needs to be set because it normally pulls it in from config which is not set up in tests
+            client.BaseAddress = new System.Uri("http://localhost");
+
+            var userService = new UserService(client);
+
+            var response = await userService.GetUserDetails("2");
+
+            Assert.Equal("First", response?.FirstName);
         }
     }
 }
