3fd99c6fb3314952c5adbc0ac209f7770dd404e6
Jeff <jeff.wong1990@gmail.com>
2022-04-07 12:35:29 +1000

    Migrated over HomeController, SettingsController and Services project

---
+++ CoreUpgrade/MyDeal.TechTest/MyDeal.TechTest/Controllers/SettingsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyDeal.TechTest.Controllers
+{
+    public class SettingsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Json(new SettingsVm
+            {
+                User = UserService.GetUserDetails("2")?.Data,
+                Message = ConfigurationManager.AppSettings["Settings:Message"]
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
